1e73ae11da56fe1acb040dde95bff2f7b864e591
rajvardhan <rajvardhanjagtap2000@gmail.com>
2023-05-28 00:03:37 +0530

    payment set

--- frontend/payment_module/src/app/connect.service.ts
+++ frontend/payment_module/src/app/connect.service.ts
@@ -8,9 +8,14 @@ import { Observable } from 'rxjs';
 export class ConnectService {
 
   setUrl="http://localhost:3000/create-account"
+  
+  setUrlPayment=""
   constructor(private http : HttpClient) { }
 
   setDataLog(data:any):Observable<any[]>{
     return this.http.post<any>(this.setUrl,data);
+  }
+  setPayment(data:any):Observable<any>{
+    return this.http.post<any>(this.setUrlPayment,data)
   }
 }
